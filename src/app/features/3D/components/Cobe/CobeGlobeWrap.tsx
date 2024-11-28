5fcae997d4c9b4b34826ec4791bf4b8565161c8f
otowebdev <106678583+otowebdev@users.noreply.github.com>
2024-11-29 07:50:13 +0900

    globe model animationの追加。

---
+++ src/app/features/3D/components/Cobe/CobeGlobeWrap.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from 'react'
+import { Cobe } from "./CobeGlobe";
+
+const CobeGlobeWrap = () => {
+  return <Cobe />;
+};
+
+export default CobeGlobeWrap
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Hero from "./components/layouts/Hero/Hero";
-
+import CobeGlobeWrap from "./features/3D/components/Cobe/CobeGlobeWrap";
 
 export default function Home() {
   return (
     <>
       {/* <div> */}
-        {/* <Scene /> */}
-        <Hero />
+      {/* <Scene /> */}
+      <Hero />
+      <CobeGlobeWrap />
       {/* </div> */}
     </>
   );
