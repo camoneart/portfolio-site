70dab70b687e3a18178e2109aeaabf2489995e38
otowebdev <106678583+otowebdev@users.noreply.github.com>
2024-12-04 20:19:02 +0900

    使用してないpostsをコメントアウト

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,9 +5,9 @@ import React from "react";
 import * as Blog from "../features/blog/components/index";
 
 const BlogPage = async () => {
-  const posts = await fetchAllBlogs("http://localhost:3000/api/v1/blog/getAllBlogs", {
-    next: { revalidate: 6000 },
-  });
+  // const posts = await fetchAllBlogs("http://localhost:3000/api/v1/blog/getAllBlogs", {
+  //   next: { revalidate: 6000 },
+  // });
   return (
     <>
       <Blog.BlogImage />
