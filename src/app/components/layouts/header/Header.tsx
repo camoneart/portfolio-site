8281ea84668adf07227edf09da4d1945432387d8
aoyamadev <106678583+aoyamadev@users.noreply.github.com>
2024-12-10 20:10:27 +0900

    HeaderのLinkコンポーネントをnext-view-transitionsに変更

--- src/app/components/layouts/header/Header.tsx
+++ src/app/components/layouts/header/Header.tsx
@@ -1,20 +1,40 @@
+"use client";
+
 import React from "react";
 import styles from "./Header.module.css";
-import Navigation from "../navigation/Navigation";
-import Link from "next/link";
+import AudioPlayer from "../../AudioPlayer/AudioPlayer";
+import Gnav from "../Gnav/Gnav";
+// import Link from "next/link";
+import { Link } from 'next-view-transitions'
+import { motion } from "framer-motion";
 
 const Header = () => {
   return (
-    <header className={`font-russo w-full mx-auto max-w-screen-2xl select-none ${`w-full h-auto py-8 px-6 ${styles["header"]}`}`}>
+    <motion.header
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 0.2, duration: 1.5, ease: "easeIn" },
+      }}
+      className={`sticky top-0 left-0 z-50 w-full mx-auto max-w-screen-xxl select-none ${`w-full h-auto py-8 px-6 ${styles["header"]}`}`}
+    >
       <div className={`flex items-center justify-between ${styles["header__container"]}`}>
-        <div className={`z-20 text-2xl font-black ${styles["header__title"]}`}>
+        <div className={`text-2xl font-black ${styles["header__title"]}`}>
           <Link href="/">
             <span className={`${styles["text-gradient"]}`}>Aoyama</span>
           </Link>
         </div>
-        <Navigation />
+        <div className={styles["header__items"]}>
+          <Gnav />
+          <AudioPlayer
+            src="/audio/They.mp3"
+            size={17}
+            color="#fff"
+            initialVolume={0.7}
+          />
+        </div>
       </div>
-    </header>
+    </motion.header>
   );
 };
 
