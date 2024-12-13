b6e3d630153d75a109386d7b555564db485da6eb
aoyamadev <106678583+aoyamadev@users.noreply.github.com>
2024-12-13 21:58:12 +0900

    skillscardのアニメーションロジックをカスタムフックに分離した、

--- src/app/features/skills/components/SkillsCard/SkillsCard.tsx
+++ src/app/features/skills/components/SkillsCard/SkillsCard.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import React, { useEffect, useRef } from 'react'
-import Image from 'next/image';
-import styles from './SkillsCard.module.css';
-import gsap from 'gsap';
-import { Link } from 'next-view-transitions'
+import Image from "next/image";
+import styles from "./SkillsCard.module.css";
+import { Link } from "next-view-transitions";
+import { useSkillsCardAnimation } from "@/app/features/skills/hooks/useSkillsCardAnimation";
 
 interface CardProps {
   id: number;
@@ -22,72 +21,65 @@ interface CardProps {
   viewTransitionLogoBg: string;
 }
 
-const SkillsCard = ({ id, skillTitle, skillLink, skillLogo, label, index, viewTransitionName, viewTransitionImage, viewTransitionBg, viewTransitionLabel, viewTransitionLogoWrapper, viewTransitionLogoBg }: CardProps) => {
+const SkillsCard = ({
+  skillTitle,
+  skillLink,
+  skillLogo,
+  label,
+  index,
+  viewTransitionName,
+  viewTransitionImage,
+  viewTransitionBg,
+  viewTransitionLabel,
+  viewTransitionLogoWrapper,
+  viewTransitionLogoBg,
+}: CardProps) => {
 
-  const cardRef = useRef<HTMLElement>(null);
-
-  // GSAPを使用したカードアニメーション
-  useEffect(() => {
-    const card = cardRef.current;
-    if (!card) return; // cardRef.currentがnullの場合は処理を中断
-    const initialDelay = 0.65; // タイトルアニメーション後の初期遅延
-
-    // カードの初期状態を設定
-    // gsap.set(card, {
-    //   opacity: 0,
-    //   y: 100,
-    // });
-
-    // // カードのアニメーション
-    // gsap.to(card, {
-    //   opacity: 1,
-    //   y: 0,
-    //   duration: 1,
-    //   ease: "power2.out",
-    //   delay: initialDelay + (0.1 * index), // 初期遅延 + インデックスベースの遅延
-    //   clearProps: "transform" // アニメーション後にtransformプロパティをクリア
-    // });
-
-    // マウスムーブエフェクト
-    const handleMouseMove = (e: MouseEvent) => {
-      const cards = document.querySelectorAll(`#card`);
-      for (let i = 0; i < cards.length; i++) {
-        const card = cards[i] as HTMLElement;
-        const rect = card.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-  
-        card.style.setProperty('--mouse-x', `${x}px`);
-        card.style.setProperty('--mouse-y', `${y}px`);
-      }
-    };
-  
-    document.addEventListener('mousemove', handleMouseMove);
-  
-    return () => {
-      document.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, [index]);
+  const { cardRef } = useSkillsCardAnimation({ index });
 
   return (
     <>
-    <article  ref={cardRef} id="card" className={`${styles["skills-card"]} ${viewTransitionBg}`}>
-      <Link href={`/skills/${skillLink}`} rel="noopener noreferrer" className={`${styles["skills-card-link"]}`}>
-        <div className={styles["skills-card-content"]}>
-          <div className={`${styles["skills-logo-wrapper"]} ${viewTransitionLogoWrapper}`}>
-            <div className={`${styles["skills-logo-bg"]} ${viewTransitionLogoBg}`}>
-              <Image src={skillLogo} alt={skillTitle} className={`${styles["skills-logo"]} ${viewTransitionImage}`} width={156} height={156} />
+      <article
+        ref={cardRef}
+        id="card"
+        className={`${styles["skills-card"]} ${viewTransitionBg}`}
+      >
+        <Link
+          href={`/skills/${skillLink}`}
+          rel="noopener noreferrer"
+          className={`${styles["skills-card-link"]}`}
+        >
+          <div className={styles["skills-card-content"]}>
+            <div
+              className={`${styles["skills-logo-wrapper"]} ${viewTransitionLogoWrapper}`}
+            >
+              <div
+                className={`${styles["skills-logo-bg"]} ${viewTransitionLogoBg}`}
+              >
+                <Image
+                  src={skillLogo}
+                  alt={skillTitle}
+                  className={`${styles["skills-logo"]} ${viewTransitionImage}`}
+                  width={156}
+                  height={156}
+                />
+              </div>
+            </div>
+            <div className={styles["skills-info"]}>
+              <h2 className={`${styles["skills-title"]} ${viewTransitionName}`}>
+                {skillTitle}
+              </h2>
+              <div
+                className={`${styles["skills-label"]} ${viewTransitionLabel}`}
+              >
+                {label}
+              </div>
             </div>
           </div>
-          <div className={styles["skills-info"]}>
-            <h2 className={`${styles["skills-title"]} ${viewTransitionName}`}>{skillTitle}</h2>
-            <div className={`${styles["skills-label"]} ${viewTransitionLabel}`}>{label}</div>
-          </div>
-        </div>
-      </Link>
-    </article>
+        </Link>
+      </article>
     </>
-  )
-}
+  );
+};
 
 export default SkillsCard;
---
+++ src/app/features/skills/hooks/useSkillsCardAnimation.ts
@@ -0,0 +1,53 @@
+import React, { useEffect, useRef } from 'react'
+import gsap from 'gsap';
+
+interface UseSkillsAnimationProps {
+  index: number;
+}
+
+export const useSkillsCardAnimation = ({ index }: UseSkillsAnimationProps) => {
+  const cardRef = useRef<HTMLElement>(null);
+  // GSAPを使用したカードアニメーション
+  useEffect(() => {
+    const card = cardRef.current;
+    if (!card) return; // cardRef.currentがnullの場合は処理を中断
+    const initialDelay = 0.65; // タイトルアニメーション後の初期遅延
+
+    // カードの初期状態を設定
+    gsap.set(card, {
+      opacity: 0,
+      y: 100,
+    });
+
+    // カードのアニメーション
+    gsap.to(card, {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      ease: "power2.out",
+      delay: initialDelay + 0.1 * index, // 初期遅延 + インデックスベースの遅延
+      clearProps: "transform", // アニメーション後にtransformプロパティをクリア
+    });
+
+    // マウスムーブエフェクト
+    const handleMouseMove = (e: MouseEvent) => {
+      const cards = document.querySelectorAll(`#card`);
+      for (let i = 0; i < cards.length; i++) {
+        const card = cards[i] as HTMLElement;
+        const rect = card.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+      }
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [index]);
+  return { cardRef };
+}
