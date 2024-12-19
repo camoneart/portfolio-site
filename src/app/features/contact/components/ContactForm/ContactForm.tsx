fdd8a13d209f2f6d047ccb2e87f1056a4e343e6f
aoyamadev <106678583+aoyamadev@users.noreply.github.com>
2024-12-19 22:25:53 +0900

    FormのローダーUIを修正

--- src/app/features/contact/components/ContactForm/ContactForm.tsx
+++ src/app/features/contact/components/ContactForm/ContactForm.tsx
@@ -6,7 +6,8 @@ import styles from "./ContactForm.module.css";
 
 import useContactForm from "@/app/features/contact/hooks/useContactForm";
 
-import { ClipLoader } from "react-spinners";
+// import { ClipLoader } from "react-spinners";
+import { ProgressBar } from "react-loader-spinner";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -27,51 +28,50 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 
-import toast, { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
 
 // toast UI
 // const notify = () => toast.success('送信に成功しました！');
-const notify = () => toast.custom((t) => (
-  <div
-    className={`${
-      t.visible ? 'animate-enter' : 'animate-leave'
-    } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
-  >
-    <div className="flex-1 w-0 p-4">
-      <div className="flex items-start">
-        <div className="flex-shrink-0 pt-0.5">
-          <img
-            className="h-10 w-10 rounded-full"
-            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixqx=6GHAjsWpt9&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
-            alt=""
-          />
-        </div>
-        <div className="ml-3 flex-1">
-          <p className="text-sm font-medium text-gray-900">
-            Emilia Gates
-          </p>
-          <p className="mt-1 text-sm text-gray-500">
-            Sure! 8:30pm works great!
-          </p>
+const notify = () =>
+  toast.custom((t) => (
+    <div
+      className={`${
+        t.visible ? "animate-enter" : "animate-leave"
+      } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+    >
+      <div className="flex-1 w-0 p-4">
+        <div className="flex items-start">
+          <div className="flex-shrink-0 pt-0.5">
+            <img
+              className="h-10 w-10 rounded-full"
+              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixqx=6GHAjsWpt9&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+              alt=""
+            />
+          </div>
+          <div className="ml-3 flex-1">
+            <p className="text-sm font-medium text-gray-900">Emilia Gates</p>
+            <p className="mt-1 text-sm text-gray-500">
+              Sure! 8:30pm works great!
+            </p>
+          </div>
         </div>
       </div>
+      <div className="flex border-l border-gray-200">
+        <button
+          onClick={() => toast.dismiss(t.id)}
+          className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        >
+          Close
+        </button>
+      </div>
     </div>
-    <div className="flex border-l border-gray-200">
-      <button
-        onClick={() => toast.dismiss(t.id)}
-        className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-      >
-        Close
-      </button>
-    </div>
-  </div>
-));
+  ));
 
 const ContactForm = () => {
   const { form, onSubmit } = useContactForm();
 
   useEffect(() => {
-    if(form.formState.isSubmitSuccessful) {
+    if (form.formState.isSubmitSuccessful) {
       notify();
     }
   }, [form.formState.isSubmitSuccessful]);
@@ -96,7 +96,11 @@ const ContactForm = () => {
                       <TooltipProvider>
                         <Tooltip>
                           <TooltipTrigger asChild>
-                            <Input placeholder="First & Last Name" {...field} className="px-3 py-6 md:text-sm" />
+                            <Input
+                              placeholder="First & Last Name"
+                              {...field}
+                              className="px-3 py-6 md:text-sm"
+                            />
                           </TooltipTrigger>
                           <TooltipContent>
                             <p>お名前を入力してください</p>
@@ -118,7 +122,11 @@ const ContactForm = () => {
                       <TooltipProvider>
                         <Tooltip>
                           <TooltipTrigger asChild>
-                            <Input placeholder="Your email address" {...field} className="px-3 py-6 md:text-sm" />
+                            <Input
+                              placeholder="Your email address"
+                              {...field}
+                              className="px-3 py-6 md:text-sm"
+                            />
                           </TooltipTrigger>
                           <TooltipContent>
                             <p>メールアドレスを入力してください</p>
@@ -141,7 +149,11 @@ const ContactForm = () => {
                     <TooltipProvider>
                       <Tooltip>
                         <TooltipTrigger asChild>
-                          <Input placeholder="Subject" {...field} className="px-3 py-6 md:text-sm" />
+                          <Input
+                            placeholder="Subject"
+                            {...field}
+                            className="px-3 py-6 md:text-sm"
+                          />
                         </TooltipTrigger>
                         <TooltipContent>
                           <p>件名を入力してください</p>
@@ -210,11 +222,13 @@ const ContactForm = () => {
               )}
             />
             <div className="contact-form-send-btn-container">
-              <div className="contact-form-send-btn-bg">
-                <Button type="submit" disabled={form.formState.isSubmitting}>
-                  {form.formState.isSubmitting ? <ClipLoader /> : "Send"}
-                </Button>
-              </div>
+              {form.formState.isSubmitting
+              ? <ProgressBar height="100" width="100" barColor="#ca510c99" borderColor="#bd4d0d" />
+              :
+                <div className="contact-form-send-btn-bg">
+                  <Button type="submit" disabled={form.formState.isSubmitting}>Send</Button>
+                </div>
+              }
             </div>
           </form>
         </Form>
