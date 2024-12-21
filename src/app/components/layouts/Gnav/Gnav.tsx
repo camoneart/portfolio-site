b41b06003f5322b539560b07e6ae8ba0ab85363a
aoyamadev <106678583+aoyamadev@users.noreply.github.com>
2024-12-21 17:29:42 +0900

    gnavにhover-unクラスを追加して、ホバーアニメーションを実装

--- src/app/components/layouts/Gnav/Gnav.tsx
+++ src/app/components/layouts/Gnav/Gnav.tsx
@@ -12,16 +12,16 @@ const Gnav = ({ isOpen, toggleMenu }: GnavProps) => {
     <nav className={`font-russo ${styles["gnav"]} ${isOpen ? styles["gnav--open"] : ""}`}>
       <ul className={`font-black ${styles["gnav__list"]} ${isOpen ? styles["gnav__list--open"] : ""}`}>
         <li className={`${styles["gnav__item"]} ${isOpen ? styles["gnav__item--open"] : ""}`}>
-          <Link className={`${styles["gnav__link"]} ${isOpen ? styles["gnav__link--open"] : ""}`} href="/about" onClick={toggleMenu}>About</Link>
+          <Link className={`${styles["gnav__link"]} ${styles["hover-un"]} ${isOpen ? styles["gnav__link--open"] : ""}`} href="/about" onClick={toggleMenu}>About</Link>
         </li>
         <li className={`${styles["gnav__item"]} ${isOpen ? styles["gnav__item--open"] : ""}`}>
-          <Link className={`${styles["gnav__link"]} ${isOpen ? styles["gnav__link--open"] : ""}`} href="/skills" onClick={toggleMenu}>Skills</Link>
+          <Link className={`${styles["gnav__link"]} ${styles["hover-un"]} ${isOpen ? styles["gnav__link--open"] : ""}`} href="/skills" onClick={toggleMenu}>Skills</Link>
         </li>
         <li className={`${styles["gnav__item"]} ${isOpen ? styles["gnav__item--open"] : ""}`}>
-          <Link className={`${styles["gnav__link"]} ${isOpen ? styles["gnav__link--open"] : ""}`} href="/works" onClick={toggleMenu}>Works</Link>
+          <Link className={`${styles["gnav__link"]} ${styles["hover-un"]} ${isOpen ? styles["gnav__link--open"] : ""}`} href="/works" onClick={toggleMenu}>Works</Link>
         </li>
         <li className={`${styles["gnav__item"]} ${isOpen ? styles["gnav__item--open"] : ""}`}>
-          <Link className={`${styles["gnav__link"]} ${isOpen ? styles["gnav__link--open"] : ""}`} href="/contact" onClick={toggleMenu}>Contact</Link>
+          <Link className={`${styles["gnav__link"]} ${styles["hover-un"]} ${isOpen ? styles["gnav__link--open"] : ""}`} href="/contact" onClick={toggleMenu}>Contact</Link>
         </li>
       </ul>
     </nav>
