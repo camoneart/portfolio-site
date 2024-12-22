18750b0cfbe01a994f9447a2300a7846ac91fbd9
aoyamadev <106678583+aoyamadev@users.noreply.github.com>
2024-12-23 06:38:55 +0900

    motion/react経由でインポートするように変更

---
+++ src/app/components/Animation/AnimationTitle/AnimationTitle.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from "react";
+// import { motion } from "framer-motion";
+import { motion } from "motion/react"
+import styles from "./AnimationTitle.module.css";
+
+const AnimationTitle = ({ title, subTitle }: { title: string, subTitle: string }) => {
+  return (
+    <motion.hgroup
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 0.2, duration: 1.5, ease: "easeIn" },
+      }}
+      className={`flex flex-col items-center justify-center gap-2 select-none ${styles["title-group"]}`}
+    >
+      <h1 className={`font-russo order-2 font-black tracking-wider ${styles["title"]} ${styles["motion-title"]}`}>
+        {title}
+      </h1>
+      <p className={`order-1 font-black tracking-wider ${styles["sub-title"]}`}>{subTitle}</p>
+    </motion.hgroup>
+  );
+};
+
+export default AnimationTitle;
+  
