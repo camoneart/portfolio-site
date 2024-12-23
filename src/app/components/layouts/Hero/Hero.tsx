c067fe004b0ffef8f0da11ff6c0ea45f4fc6bf22
aoyamadev <106678583+aoyamadev@users.noreply.github.com>
2024-12-24 04:01:14 +0900

    motionのインポートをmotion/react経由にした

--- src/app/components/layouts/Hero/Hero.tsx
+++ src/app/components/layouts/Hero/Hero.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import styles from "./Hero.module.css";
-import { motion } from "framer-motion";
+import { motion } from "motion/react"
 
 const Hero = () => {
   return (
@@ -14,21 +14,13 @@ const Hero = () => {
       }}
       className={`font-russo h-full grid  ${styles["hero"]}`}
     >
-      <div
-        className={`pt-6 pb-12 flex flex-col items-center justify-end ${styles["hero__container"]}`}
-      >
-        <hgroup
-          className={`text-center flex flex-col gap-3 md:gap-6 select-none ${styles["hero__hgroup"]}`}
-        >
-          <h1
-            className={`z-20 text-5xl md:text-8xl tracking-wide font-black ${styles["hero__title"]}`}
-          >
+      <div className={`pt-6 pb-12 flex flex-col items-center justify-end ${styles["hero__container"]}`} >
+        <hgroup className={`text-center flex flex-col gap-3 md:gap-6 select-none ${styles["hero__hgroup"]}`} >
+          <h1 className={`z-20 text-5xl md:text-8xl tracking-wide font-black ${styles["hero__title"]}`} >
             <span className={`${styles["text-gradient"]}`}>Aoyama</span>
           </h1>
-          <p
-            className={`z-20 text-xl md:text-2xl tracking-wider font-black ${styles["hero__description"]}`}
-          >
-            Creative Web Developer
+          <p className={`z-20 text-xl md:text-2xl tracking-wider font-black ${styles["hero__description"]}`} >
+            Creative Web Engineer
           </p>
         </hgroup>
       </div>
