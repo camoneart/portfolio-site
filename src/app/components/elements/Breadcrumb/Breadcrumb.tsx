8e925c63886348ba72db907ea3bd8f757b73cfc5
aoyamadev <106678583+aoyamadev@users.noreply.github.com>
2024-12-24 07:25:51 +0900

    パンくずリストにmotionを追加して出現アニメーションを実装

--- src/app/components/elements/Breadcrumb/Breadcrumb.tsx
+++ src/app/components/elements/Breadcrumb/Breadcrumb.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
 import styles from "./Breadcrumb.module.css";
+import { motion } from "motion/react";
 
 interface BreadcrumbProps {
   title: string;
@@ -11,7 +14,7 @@ interface BreadcrumbProps {
 
 const truncateText = (text: string, maxLength: number): string => {
   if (text.length <= maxLength) return text;
-  return text.slice(0, maxLength) + '...';
+  return text.slice(0, maxLength) + "...";
 };
 
 const Breadcrumb: React.FC<BreadcrumbProps> = ({
@@ -23,21 +26,39 @@ const Breadcrumb: React.FC<BreadcrumbProps> = ({
   const truncatedTitle = truncateText(title, maxLength);
 
   return (
-    <nav aria-label="パンくずリスト" className={`font-russo ${styles["breadcrumb"]}`}>
+    <motion.nav
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 0.5, duration: 1.2, ease: "easeIn" }
+      }}
+      aria-label="パンくずリスト"
+      className={`font-russo ${styles["breadcrumb"]}`}
+    >
       <ol className={`${styles["breadcrumb__list"]}`}>
         <li className={styles["breadcrumb__item"]}>
-          <Link href="/" className={`${styles["breadcrumb__link"]} ${styles["hover-un"]}`}>Home</Link>
+          <Link
+            href="/"
+            className={`${styles["breadcrumb__link"]} ${styles["hover-un"]}`}
+          >
+            Home
+          </Link>
         </li>
         {parentPath && parentLabel && (
           <li className={styles["breadcrumb__item"]}>
-            <Link href={parentPath} className={`${styles["breadcrumb__link"]} ${styles["hover-un"]}`}>{parentLabel}</Link>
+            <Link
+              href={parentPath}
+              className={`${styles["breadcrumb__link"]} ${styles["hover-un"]}`}
+            >
+              {parentLabel}
+            </Link>
           </li>
         )}
         <li className={styles["breadcrumb__item"]}>
           <span title={title}>{truncatedTitle}</span>
         </li>
       </ol>
-    </nav>
+    </motion.nav>
   );
 };
 
