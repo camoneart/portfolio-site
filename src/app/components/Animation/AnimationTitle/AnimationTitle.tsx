07d17b3b23327fe0f20248337e81270b8b84278b
aoyamadev <106678583+aoyamadev@users.noreply.github.com>
2024-12-25 05:50:29 +0900

    各ページのタイトルにmotionでanimationを実装した状態

--- src/app/components/Animation/AnimationTitle/AnimationTitle.tsx
+++ src/app/components/Animation/AnimationTitle/AnimationTitle.tsx
@@ -10,20 +10,25 @@ const AnimationTitle = ({ title, subTitle }: { title: string, subTitle: string }
       className={`flex flex-col items-center justify-center gap-2 select-none ${styles["title-group"]}`}
     >
       <motion.h1
-        initial={{ y: 50, opacity: 0 }}
+        // initial={{ opacity: 0, y: 50 }}
+        initial={{ opacity: 0 }}
         animate={{
-          y: 0,
           opacity: 0.15,
-          transition: { type: "spring", stiffness: 100, damping: 12, delay: 0.2, duration: 1, ease: "easeInOut" },
+          // y: 0,
+          // transition: { type: "spring", stiffness: 100, damping: 12, delay: 0.2, duration: 1, ease: "easeInOut" },
+          transition: { delay: 0.2, duration: 1, ease: "easeInOut" },
         }}
         className={`font-russo order-2 font-black ${styles["title"]} ${styles["motion-title"]}`}>
         {title}
       </motion.h1>
       <motion.p
+        // initial={{ opacity: 0, y: 60 }}
         initial={{ opacity: 0 }}
         animate={{
           opacity: 1,
-          transition: { delay: 0.5, duration: 1.4, ease: "easeInOut" },
+          // y: 0,
+          // transition: { type: "spring", stiffness: 100, damping: 12, delay: 0.25, duration: 1.4, ease: "easeInOut" },
+          transition: { delay: 0.3, duration: 1, ease: "easeInOut" },
         }}
         className={`order-1 font-black ${styles["sub-title"]}`}>
         {subTitle}
@@ -33,4 +38,3 @@ const AnimationTitle = ({ title, subTitle }: { title: string, subTitle: string }
 };
 
 export default AnimationTitle;
-  
