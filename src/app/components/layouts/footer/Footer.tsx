b46581b6336147f5a7696f608d950b65dafa57a9
aoyamadev <106678583+aoyamadev@users.noreply.github.com>
2024-12-27 14:09:21 +0900

    footerのz-indexを削除

--- src/app/components/layouts/footer/Footer.tsx
+++ src/app/components/layouts/footer/Footer.tsx
@@ -1,13 +1,20 @@
+"use client";
+
 import React from "react";
 import styles from "./Footer.module.css";
+import { motion } from "motion/react"
 
 const Footer = () => {
   return (
     <footer className={`font-russo select-none ${styles["footer"]}`}>
-      <div className={`p-4 grid place-items-center ${styles["footer__container"]}`}>
-        <p className={`z-20 ${styles["footer__text"]}`}>
+      <div className={`p-5 grid place-items-center ${styles["footer__container"]}`}>
+        <p className={`${styles["footer__text"]}`}>
           <small className={`text-xs font-bold ${styles["footer__copyright"]}`}>
-            <span className={`${styles["text-gradient"]}`}>&copy; 2024 Aoyama</span>
+            <motion.span initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 0.2, duration: 1.5, ease: "easeIn" },
+      }}className={`${styles["text-gradient"]}`}>&copy; 2024 Aoyama</motion.span>
           </small>
         </p>
       </div>
