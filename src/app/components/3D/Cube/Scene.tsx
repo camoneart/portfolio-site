97acf1b3a6d6f961c672224b609688f759f4ef17
aoyamadev <106678583+aoyamadev@users.noreply.github.com>
2025-01-01 04:06:49 +0900

    キューブの位置調整

--- src/app/components/3D/Cube/Scene.tsx
+++ src/app/components/3D/Cube/Scene.tsx
@@ -26,7 +26,7 @@ const Scene = () => {
     <>
       <PerspectiveCamera
         makeDefault
-        position={[90, 65, 35]}
+        position={[90, 65, 38]}
         fov={50}
       />
       <ambientLight intensity={0.3} />
