37b6e1a1bcbae2b3be7164a54cc96cc296de218d
aoyamadev <106678583+aoyamadev@users.noreply.github.com>
2025-01-06 21:12:20 +0900

    next.config.mjsの設定を修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': '/src'
+    };
+    return config;
+  }
 };
 
 export default nextConfig;
