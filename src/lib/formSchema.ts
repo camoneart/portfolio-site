a0d277d0021a9bcf7fabb62593d00f5e8d7838a5
aoyamadev <106678583+aoyamadev@users.noreply.github.com>
2025-01-12 11:08:02 +0900

    formSchemaからfile関連の定義を削除した

--- src/lib/formSchema.ts
+++ src/lib/formSchema.ts
@@ -1,15 +1,5 @@
 import { z } from "zod";
 
-const MAX_MB = 10;
-const MAX_FILE_SIZE = MAX_MB * 1024 * 1024;
-const ACCEPTED_FILE_TYPES = [
-  "image/jpeg",
-  "image/png",
-  "image/gif",
-  "image/webp",
-  "application/pdf",
-];
-
 const formSchema = z.object({
   username: z
     .string()
@@ -25,30 +15,6 @@ const formSchema = z.object({
     .string()
     .min(10, { message: "本文は10文字以上で入力してください" })
     .max(4000, { message: "本文は4000文字以内で入力してください。" }),
-  file: z
-    .custom<FileList>()
-    .optional()
-    .superRefine((files, ctx) => {
-      // ファイルが選択されていない場合は検証をスキップ
-      if (!files || files.length === 0) return;
-
-      // ファイルが選択された場合のみ、以下の検証を実行
-      if (files[0].size > MAX_FILE_SIZE) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message: `ファイルサイズは${MAX_MB}MB以下にしてください。`,
-        });
-      }
-
-      if (!ACCEPTED_FILE_TYPES.includes(files[0].type)) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message: "jpeg, png, gif, webp, pdfのみ添付できます。",
-        });
-      }
-    })
 });
 
 export default formSchema;
-
-
