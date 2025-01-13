c49b98c55649fb5a5c7d3c0ad08fcb832d40b170
aoyamadev <106678583+aoyamadev@users.noreply.github.com>
2025-01-13 21:33:17 +0900

    CustomParagraphコンポーネントを追加

---
+++ src/app/components/elements/CustomParagraph/CustomParagraph.tsx
@@ -0,0 +1,21 @@
+// CustomParagraph.tsx
+import React from 'react';
+import { Components } from 'react-markdown';
+import styles from './CustomParagraph.module.css';
+
+export const CustomParagraph: Components['p'] = ({ children, ...props }) => {
+  if (typeof children === 'string') {
+    // 文字列の先頭に"空白"があるかチェック
+    const match = children.match(/^(　)(.*)/);
+    if (match) {
+      const [, dot, rest] = match;  // [全体のマッチ, 空白の部分, 残りのテキスト]
+      return (
+        <p {...props}>
+          <span className={styles["dot-marker"]}>{dot}</span>
+          {rest}
+        </p>
+      );
+    }
+  }
+  return <p {...props}>{children}</p>;
+};
