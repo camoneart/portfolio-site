30344bcb24498f0648529f3fb51af61ada6a9758
aoyamadev <106678583+aoyamadev@users.noreply.github.com>
2025-01-14 19:16:55 +0900

    管理者向け自動配信メールの見出しh2をh3に変更

--- src/app/features/contact/components/AdminEmailTemplate/AdminEmailTemplate.tsx
+++ src/app/features/contact/components/AdminEmailTemplate/AdminEmailTemplate.tsx
@@ -11,7 +11,7 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   content,
 }) => (
   <div>
-    <h2>{username}様から、お問い合わせが届きました。</h2>
+    <h3>{username}様から、お問い合わせが届きました。</h3>
     <h3>お問い合わせ内容は以下の通りです。</h3>
     <p>{content}</p>
   </div>
