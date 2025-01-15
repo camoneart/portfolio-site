341e78b06adaf8273e49733f2f71fe49a119f184
aoyamadev <106678583+aoyamadev@users.noreply.github.com>
2025-01-15 16:16:36 +0900

    works詳細ページのメタデータdescriptionを修正

--- src/app/works/[id]/metadata.ts
+++ src/app/works/[id]/metadata.ts
@@ -14,6 +14,6 @@ export async function generateWorkMetadata({ params }: { params: { id: string } 
 
   return {
     title: `${workData.workTitle}`,
-    description: `このページでは、制作実績：「${workData.workTitle}」の詳細についてご紹介いたします。`,
+    description: `このページでは、制作実績：「${workData.workTitle}」の詳細についてご覧いただけます。`,
   };
 }
