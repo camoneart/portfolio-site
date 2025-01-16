3919f2214c85e0a03a06cc3aa2d92a4794cea74b
aoyamadev <106678583+aoyamadev@users.noreply.github.com>
2025-01-16 21:38:16 +0900

    mobileDelayの速度を調整

--- src/app/features/footer/hooks/useFooterResponsiveAnimation.tsx
+++ src/app/features/footer/hooks/useFooterResponsiveAnimation.tsx
@@ -1,7 +1,7 @@
 import useResponsiveAnimation from '@/app/hooks/useResponsiveAnimation';
 
 const useFooterResponsiveAnimation = () => {
-  return useResponsiveAnimation({ mobileDelay: 2.2, desktopDelay: 2.8 });
+  return useResponsiveAnimation({ mobileDelay: 2, desktopDelay: 2.8 });
 };
 
 export default useFooterResponsiveAnimation;
