e0e08aa8496e03e5f9eec7b4b7b04fcb7177f778
aoyamadev <106678583+aoyamadev@users.noreply.github.com>
2025-01-18 07:43:18 +0900

    useFooterResponsiveAnimationのdesktopDelayを1.3に変更

--- src/app/features/footer/hooks/useFooterResponsiveAnimation.tsx
+++ src/app/features/footer/hooks/useFooterResponsiveAnimation.tsx
@@ -1,7 +1,7 @@
 import useResponsiveAnimation from '@/app/hooks/useResponsiveAnimation';
 
 const useFooterResponsiveAnimation = () => {
-  return useResponsiveAnimation({ mobileDelay: 2, desktopDelay: 2.8 });
+  return useResponsiveAnimation({ mobileDelay: 2, desktopDelay: 1.3 });
 };
 
 export default useFooterResponsiveAnimation;
