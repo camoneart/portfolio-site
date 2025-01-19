63c14411fe821c5ebf3c0e1071379a837d634e79
aoyamadev <106678583+aoyamadev@users.noreply.github.com>
2025-01-20 00:21:20 +0900

    StairsTransitionを無効に。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 import Hero from "./components/layouts/Hero/Hero";
 import ElegantFloatingCubes from "./components/3D/Cube/ElegantFloatingCubes";
-import StairsTransition from "./components/Animation/StairsTransition/StairsTransition";
+// import StairsTransition from "./components/Animation/StairsTransition/StairsTransition";
 
 export default function Home() {
   return (
     <>
-      <StairsTransition>
+      {/* <StairsTransition> */}
         <div className="absolute inset-0 z-10 h-full w-full">
           <ElegantFloatingCubes />
         </div>
         <Hero />
-      </StairsTransition>
+      {/* </StairsTransition> */}
     </>
   );
 }
