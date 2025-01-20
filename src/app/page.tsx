34a22cdc4e7cacf4916654017fba7af6724edcbf
aoyamadev <106678583+aoyamadev@users.noreply.github.com>
2025-01-20 21:53:30 +0900

    トップページから、StairsTransitionコンポーネントを削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 import Hero from "./components/layouts/Hero/Hero";
 import ElegantFloatingCubes from "./components/3D/Cube/ElegantFloatingCubes";
-import StairsTransition from "./components/Animation/StairsTransition/StairsTransition";
 
 export default function Home() {
   return (
     <>
-      <StairsTransition>
-        <div className="absolute inset-0 z-10 h-full w-full">
-          <ElegantFloatingCubes />
-        </div>
-        <Hero />
-      </StairsTransition>
+      <div className="absolute inset-0 z-10 h-full w-full">
+        <ElegantFloatingCubes />
+      </div>
+      <Hero />
     </>
   );
 }
