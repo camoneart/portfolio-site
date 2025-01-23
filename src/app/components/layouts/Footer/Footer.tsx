ba253277a924849272a5de251fb8a8395d3afbc5
aoyamadev <106678583+aoyamadev@users.noreply.github.com>
2025-01-24 02:45:24 +0900

    remove(Footer)：Footerコンポーネントのmotionアニメーションを削除。
    既存のmotionによるアニメーション実装を削除。
    - トップページでstairsアニメーションを削除して不要になったため。

--- src/app/components/layouts/Footer/Footer.tsx
+++ src/app/components/layouts/Footer/Footer.tsx
@@ -2,26 +2,18 @@
 
 import React from "react";
 import styles from "./Footer.module.css";
-import { motion } from "motion/react";
-import useFooterResponsiveAnimation from "@/app/features/footer/hooks/useFooterResponsiveAnimation";
 
 export default function Footer() {
-  const { animationProps } = useFooterResponsiveAnimation();
-
   return (
     <footer className={`font-russo select-none ${styles["footer"]}`}>
-      <div
-        className={`p-5 grid place-items-center ${styles["footer__container"]}`}
-      >
-        <motion.p
-          {...animationProps}
-          className={`z-20 ${styles["footer__text"]}`}>
+      <div className={`p-5 grid place-items-center ${styles["footer__container"]}`}>
+        <p className={`z-20 ${styles["footer__text"]}`}>
           <small className={`text-xs font-bold ${styles["footer__copyright"]}`}>
             <span className={`${styles["text-gradient"]}`}>
               &copy; 2024 Aoyama
             </span>
           </small>
-        </motion.p>
+        </p>
       </div>
     </footer>
   );
