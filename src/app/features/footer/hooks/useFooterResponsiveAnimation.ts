e35f3ac21d1ac6c918709c82b96a1fde30c630f6
aoyamadev <106678583+aoyamadev@users.noreply.github.com>
2025-01-24 02:48:26 +0900

    remove(hooks): Footerのレスポンシブアニメーション用カスタムフックを削除
    useFooterResponsiveAnimationフックを完全に削除しました。
    - トップページでstairsアニメーションを削除して不要になったため。

--- src/app/features/footer/hooks/useFooterResponsiveAnimation.ts
+++
@@ -1,7 +0,0 @@
-import useResponsiveAnimation from '@/app/hooks/useResponsiveAnimation';
-
-const useFooterResponsiveAnimation = () => {
-  return useResponsiveAnimation({ mobileDelay: 2, desktopDelay: 1.4 });
-};
-
-export default useFooterResponsiveAnimation;
