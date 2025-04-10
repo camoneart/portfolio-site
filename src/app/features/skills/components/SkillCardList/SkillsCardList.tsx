c58d24e20dd39acf11cfe15475af14f14e7c5e49
aoyamadev <106678583+peterartworkdev@users.noreply.github.com>
2025-04-10 23:28:17 +0900

    feat(SkillsCardList): スキルカードリストコンポーネントを追加
    - 新しいCSSモジュールを作成し、スキルカードのレイアウトを定義しました。
    - スキルデータをマッピングして、各スキルカードを表示するReactコンポーネントを実装しました。

---
+++ src/app/features/skills/components/SkillCardList/SkillsCardList.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./SkillsCardList.module.css";
+import SkillsCard from "../SkillCard/SkillCard";
+import { skillsData } from "@/app/skills/components/Skills/Skills";
+
+const SkillsCardList = () => {
+  return (
+    <>
+      <div className={styles["skills-layout"]}>
+        {skillsData.map((skillData, index) => (
+          <SkillsCard key={skillData.id} {...skillData} index={index} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SkillsCardList;
