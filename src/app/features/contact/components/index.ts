c41a1017a04731ee6ebe98ef2cbfb2483020e498
camone <106678583+camoneart@users.noreply.github.com>
2026-01-22 14:50:56 +0900

    chore: 不要なコメントを削除

--- src/app/features/contact/components/index.ts
+++ src/app/features/contact/components/index.ts
@@ -2,7 +2,3 @@ import ContactForm from "./ContactForm/ContactForm";
 import ContactSection from "./ContactSection/ContactSection";
 
 export { ContactForm, ContactSection };
-
-// Email Templates are server-only and should be imported directly:
-// import { AdminEmailTemplate } from "@/app/features/contact/components/AdminEmailTemplate/AdminEmailTemplate";
-// import { AutoReplyEmailTemplate } from "@/app/features/contact/components/AutoReplyEmailTemplate/AutoReplyEmailTemplate";
